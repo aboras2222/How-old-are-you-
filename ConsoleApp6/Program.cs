b78b621a3d35e3e9b319c14191138007a20cf85b
Eng.Abouras <Eng.Abouras@DESKTOP-GUDPHEL>
2017-08-18 22:24:43 -0700

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime MyAge;
+			Console.Write("Enter your Birthday in Form DD/MM/YYYY:");
+			MyAge=Convert.ToDateTime(Console.ReadLine());
+			int year = Convert.ToInt32(DateTime.Now.Subtract(MyAge).TotalDays) / 360;
+			Console.WriteLine("your Age is :" + year + "year");
+			
+			
+			Console.ReadKey();
+
+		}
+	}
+}
